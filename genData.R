33e8a17243a013c8b00589d9c573bd5138da7493
Reynaldo <gilcu2@gmail.com>
2014-07-24 16:54:50 +0200

    generate dgf graph

--- genData.R
+++ genData.R
@@ -1,8 +1,8 @@
 genNodes=function(n) {
-  nodes=NULL
+  nodes=data.frame()
   for(i in 1:n)
     nodes=rbind(nodes,data.frame(paste("U",toString(i),sep=""),Sys.time(),
-                                 0,0,0,
+                                 0,0,sample.int(100,1)[1],
                                  stringsAsFactors = FALSE))
   colnames(nodes)=c("name","time","followers","followings","posts")
   return(nodes)
@@ -10,7 +10,7 @@ genNodes=function(n) {
 
 
 genLinks=function(nodes) {
-  links=NULL
+  links=data.frame()
   n=nrow(nodes)
   for(i in 1:n)
     for(j in 1:n)
@@ -18,7 +18,7 @@ genLinks=function(nodes) {
         {
           newLink=data.frame(nodes$name[i],nodes$name[j],
                              Sys.time(),
-                             sample(1:100,1),sample(10:100,1),
+                             sample.int(100,1)[1],sample.int(100,1)[1],
                              stringsAsFactors = FALSE)
           links=rbind(links,newLink)
 
@@ -27,16 +27,27 @@ genLinks=function(nodes) {
 
           nodes$followers[j]=nodes$followers[j]+1
           nodes$followings[i]=nodes$followings[i]+1
-          nodes$posts[i]=nodes$posts[i]+newLink[4]+newLink[5]
+          print(class(nodes$posts[i]))
+          nodes$posts[[i]]=nodes$posts[[i]]+newLink[[4]]+newLink[[5]]
+          
 
       }
   colnames(links)=c("n1","n2","time","retweets","mentions")
   return(list(nodes,links)) 
 } 
 
-n=commandArgs(T)[1]
-nodes=genNodes(n)
-genLinks(nodes)
+if (length(commandArgs(T))>0) {
+  n=commandArgs(T)[1]
+  nodes=genNodes(n)
+  grafo=genLinks(nodes)
+  
+  cat("nodedef>")
+  write.table(grafo[[1]],sep=",",row.names=F)
+  cat("edgedef>")
+  write.table(grafo[[2]],sep=",",row.names=F)
+  } else 
+      print("genData <number of vertex>")
+
 
 # gdf output
 #print("nodedef> \"name\",\"time\",\"followers\",\"followings\",\"posts\" ")
